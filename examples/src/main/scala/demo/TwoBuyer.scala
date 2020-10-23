0d6d3cb17ad30901539b20e308768b682b112b55
Alceste Scalas <alcsc@dtu.dk>
2020-10-23 12:07:40 +0200

    Add experiments with match types and for comprehension

---
+++ examples/src/main/scala/demo/TwoBuyer.scala
@@ -0,0 +1,192 @@
+// Effpi - verified message-passing programs in Dotty
+// Copyright 2020 Alceste Scalas
+// Released under the MIT License: https://opensource.org/licenses/MIT
+
+// This is the two-buyer protocol example from session types literature,
+// implemented using match types
+package effpi.examples.demo.twobuyuer
+
+import scala.concurrent.duration.Duration
+
+import effpi.process._
+import effpi.channel.{Channel, InChannel, OutChannel}
+import java.time.LocalDate
+
+package types {
+  import effpi.process.dsl._
+
+  // Message classes
+  case class Quote(amount: Int)
+  case class NotAvailable()
+
+  case class Contrib(amount: Int)
+  case class Cancel()
+  case class OK()
+  case class Negotiate()
+
+  case class Buy(address: String)
+  case class Confirm(delivery: LocalDate)
+  
+  // Seller protocol
+  type Seller[CTitle <: InChannel[String],
+              CQuote1 <: OutChannel[Quote | NotAvailable],
+              CQuote2 <: OutChannel[Quote | NotAvailable],
+              CResp <: InChannel[Buy | Cancel],
+              CConf <: OutChannel[Confirm]] =
+    In[CTitle, String, String =>
+        (Out[CQuote1, NotAvailable] >>: Out[CQuote2, NotAvailable])
+        |
+        TrySell[CQuote1, CQuote2, CResp, CConf]
+      ]
+  
+  type TrySell[CQuote1 <: OutChannel[Quote | NotAvailable],
+               CQuote2 <: OutChannel[Quote | NotAvailable],
+               CResp <: InChannel[Buy | Cancel],
+               CConf <: OutChannel[Confirm]] =
+    (Out[CQuote1, Quote] >>: Out[CQuote2, Quote] >>:
+      In[CResp, Buy|Cancel, (x: Buy|Cancel) => SellerMatch[x.type, CConf]])
+  
+  type SellerMatch[X <: Buy|Cancel, CConf <: OutChannel[Confirm]] <: Process = X match {
+    case Buy => Out[CConf, Confirm]
+    case Cancel => PNil
+  }
+
+  // Alice's protocol
+  type Alice[CTitle <: OutChannel[String],
+             CQuote <: InChannel[Quote | NotAvailable],
+             CBobContrib <: OutChannel[Contrib|Cancel],
+             CBobResp <: InChannel[OK|Cancel|Negotiate],
+             CResp <: OutChannel[Buy|Cancel],
+             CConf <: InChannel[Confirm]] =
+    Out[CTitle, String] >>:
+    In[CQuote, Quote|NotAvailable, (x: Quote|NotAvailable) =>
+      QuoteMatch[x.type, CBobContrib, CBobResp, CResp, CConf]
+    ]
+
+  type QuoteMatch[X <: Quote|NotAvailable,
+                  CBobContrib <: OutChannel[Contrib|Cancel],
+                  CBobResp <: InChannel[OK|Cancel|Negotiate],
+                  CResp <: OutChannel[Buy | Cancel],
+                  CConf <: InChannel[Confirm]] <: Process = X match {
+    case NotAvailable => Out[CBobContrib, Cancel]
+    case Quote => Rec[RecX,
+      (Out[CBobContrib, Cancel] >>: Out[CResp, Cancel])
+      |
+      (Out[CBobContrib, Contrib] >>:
+       In[CBobResp, OK|Cancel|Negotiate, (x: OK|Cancel|Negotiate) =>
+          BobRespMatch[x.type, CResp, CConf]
+       ])
+    ]
+  }
+
+  type BobRespMatch[X <: OK|Cancel|Negotiate,
+                    CResp <: OutChannel[Buy|Cancel],
+                    CConf <: InChannel[Confirm]] <: Process = X match {
+    case Cancel => Out[CResp, Cancel]
+    case Negotiate => Loop[RecX]
+    case OK => Out[CResp, Buy] >>: In[CConf, Confirm, Confirm => PNil]
+  }
+}
+
+package implementation {
+  import types._
+  implicit val timeout: Duration = Duration("30 seconds")
+  import effpi.process.dsl._
+
+  // Seller process
+  def seller(cTitle: InChannel[String],
+             cQuote1: OutChannel[Quote | NotAvailable],
+             cQuote2: OutChannel[Quote | NotAvailable],
+             cResp: InChannel[Buy | Cancel],
+             cConf: OutChannel[Confirm]): Seller[cTitle.type, cQuote1.type, cQuote2.type, cResp.type, cConf.type] = {
+    receive(cTitle) {
+      case "Book A" => sell(50, cQuote1, cQuote2, cResp, cConf)
+      case "Book B" => sell(99, cQuote1, cQuote2, cResp, cConf)
+      case _ => send(cQuote1, NotAvailable()) >> send(cQuote2, NotAvailable())
+    }
+  }
+
+  def sell(amount: Int, cQuote1: OutChannel[Quote|NotAvailable], cQuote2: OutChannel[Quote|NotAvailable],
+           cResp: InChannel[Buy | Cancel], cConf: OutChannel[Confirm]): TrySell[cQuote1.type, cQuote2.type, cResp.type, cConf.type] = {
+    send(cQuote1, Quote(amount)) >> {
+      send(cQuote2, Quote(amount)) >>
+      receive(cResp) { (res: Buy|Cancel) => res match {
+        case b: Buy => {
+          println("Shipping to: ${b.address)")
+          send(cConf, Confirm(LocalDate.now().plusWeeks(1)))
+        }
+        case _: Cancel => nil
+      } }
+    }
+  }
+
+  // This does not compile due to https://github.com/lampepfl/dotty/issues/9999
+  /*
+  // Alice's process
+  def alice(cTitle: OutChannel[String],
+            cQuote: InChannel[Quote | NotAvailable],
+            cBobContrib: OutChannel[Contrib|Cancel],
+            cBobResp: InChannel[OK|Cancel|Negotiate],
+            cResp: OutChannel[Buy | Cancel],
+            cConf: InChannel[Confirm]): Alice[cTitle.type, cQuote.type, cBobContrib.type, cBobResp.type, cResp.type, cConf.type] = {
+    //send(cTitle, "Book A") >> {
+      receive(cQuote) { (q: Quote|NotAvailable) =>
+        handleQuote(q, cBobContrib, cBobResp, cResp, cConf)
+      }
+    //} 
+  }
+
+  def handleQuote(q: Quote|NotAvailable, cBobContrib: OutChannel[Contrib|Cancel],
+                  cBobResp: InChannel[OK|Cancel|Negotiate],
+                  cResp: OutChannel[Buy | Cancel],
+                  cConf: InChannel[Confirm]): QuoteMatch[q.type, cBobContrib.type,
+                                                         cBobResp.type, cResp.type,
+                                                         cConf.type] = q match {
+        case _: NotAvailable => send(cBobContrib, Cancel())
+        case q: Quote => rec(RecX) {
+          var iterations = 1
+          if (q.amount > 1000) {
+            send(cBobContrib, Cancel()) >> send(cResp, Cancel())
+          } else {
+            val contrib = q.amount / (iterations + 1)
+            send(cBobContrib, Contrib(contrib)) >>
+            receive(cBobResp) { (res: OK|Cancel|Negotiate) =>
+              bobRespMatch(res, cResp, cConf)
+            }
+          }
+        }
+      }
+   */
+
+  def bobRespMatch(res: OK|Cancel|Negotiate,
+                   cResp: OutChannel[Buy | Cancel],
+                   cConf: InChannel[Confirm]): BobRespMatch[res.type, cResp.type, cConf.type] = {
+       res match {
+          case _: Cancel => send(cResp, Cancel())
+          case _: Negotiate => /* iterations += 1; */ loop(RecX)
+          case _: OK => {
+            send(cResp, Buy("Street 1, Copenhagen")) >>
+            receive(cConf) { (conf: Confirm) =>
+              println("Purchase confirmed; delivery on ${conf.delivery}")
+              nil
+            }
+          }
+        }
+  }
+}
+
+// To run this example, try:
+// sbt "examples/runMain effpi.examples.demo.twobuyer.Main"
+object Main {
+  import types._
+  import implementation._
+
+  def main(): Unit = main(Array())
+
+  def main(args: Array[String]) = {
+    implicit val ps = effpi.system.ProcessSystemRunnerImproved()
+
+    Thread.sleep(5000) // Wait 5 seconds
+    ps.kill()
+  }
+}
---
+++ src/main/scala/ForComprehension.scala
@@ -0,0 +1,228 @@
+// Effpi - verified message-passing programs in Dotty
+// Copyright 2020 Alceste Scalas
+// Released under the MIT License: https://opensource.org/licenses/MIT
+
+// This file contains experiments toward an API based on for comprehension
+// (and some fiddling with match types)
+package effpi.forcomp
+
+import effpi.channel._
+
+import scala.concurrent.duration.Duration
+
+/** A process that yields a value of type A if/when it terminates */
+sealed abstract class Process[+A] {
+  def run(): A
+}
+
+/** Send a value of type A over a channel of type C */
+case class Out[C <: OutChannel[A], A](c: C, v: A) extends Process[Unit] {
+  def map[Y2](f: Unit => Y2): Yielding[Out[C,A], Y2] = impl.YieldingImpl[Out[C,A], Unit, Y2](this, f)
+  def flatMap[Y2, P2 <: Process[Y2]](f: Unit => P2): Seq[Out[C,A], P2, Y2] = {
+    impl.SeqImpl[Out[C,A], P2, Unit, Y2](this, f)
+  }
+
+  override def run(): Unit = {
+    println(s"*** Sending ${v} on channel ${c}")
+    c.send(v)
+  }
+}
+
+/** Receive a value of type A from a channel of type C */
+case class Recv[C <: InChannel[A], A](c: C)(implicit timeout: Duration) extends Process[A] {
+  def map[Y2](f: A => Y2): Yielding[Recv[C, A], Y2] = impl.YieldingImpl[Recv[C,A], A, Y2](this, f)
+  def flatMap[B, P2 <: Process[B], F <: A => P2](f: F): In[C, A, F, B] = impl.InImpl(c, f, x => x)
+
+  def apply[B, P2 <: Process[B], F <: A => P2](f: F): In[C, A, F, B] = flatMap(f)
+
+  override def run(): A = {
+    println(s"*** Receiving from channel ${c}")
+    c.receive()
+  }
+}
+
+/** Execute a process of type P1, and then a process of type P2 */
+abstract class Seq[P1 <: Process[?], P2 <: Process[Y], Y] extends Process[Y]
+
+/* Execute a process of type P and then yield a value of type Y */
+abstract class Yielding[P <: Process[?], Y] extends Process[Y]
+
+/** Receive a value of type A from a channel of type C, and continue
+  *  as a process of type F.
+  */
+abstract class In[C <: InChannel[A], A, F <: A => Process[?], Y] extends Process[Y] {
+  def map[Y3](f2: Y => Y3): In[C, A, F, Y3]
+  def flatMap[Y3, P2 <: Process[Y3]](f: Y => P2): Seq[In[C, A, F, Y], P2, Y3]
+}
+
+/** DSL: send v over channel c */
+def send[C <: OutChannel[A], A](c: C, v: A) = Out[C, A](c, v)
+
+/** DSL: receive a value from channel c */
+def receive[C <: InChannel[A], A](c: C)(implicit timeout: Duration) = Recv[C, A](c)
+
+package impl {
+  case class InImpl[C <: InChannel[A], A, F <: A => Process[Y1], Y1, Y2](c: C, andThen: F, mapf: Y1 => Y2)
+                                                                        (implicit timeout: Duration) extends In[C, A, F, Y2] {
+    def map[Y3](f2: Y2 => Y3): In[C, A, F, Y3] = InImpl[C, A, F, Y1, Y3](c, andThen, x => f2(mapf(x)))
+
+    def flatMap[Y3, P2 <: Process[Y3]](f: Y2 => P2): Seq[In[C, A, F, Y2], P2, Y3] = {
+      SeqImpl[In[C, A, F, Y2], P2, Y2, Y3](this, f)
+    }
+
+    override def run(): Y2 = {
+      println(s"*** In - step 1: receive from ${c}")
+      val v = c.receive()
+      val cont = andThen(v)
+      println(s"*** In - step 2: running continuation ${cont}")
+      mapf(cont.run())
+    }
+  }
+
+  case class YieldingImpl[P <: Process[Y], Y, Y2](process: P, f: Y => Y2) extends Yielding[P, Y2] {
+    def map[Y3](f2: Y2 => Y3): Yielding[P, Y3] = YieldingImpl[P, Y, Y3](process, (x: Y) => f2(f(x)))
+    def flatMap[Y3, P2 <: Process[Y3], F <: Y2 => P2](f2: F): Seq[P, P2, Y3] = {
+      val f3 = (x: Y) => f2(f(x))
+      SeqImpl[P, P2, Y, Y3](process, f3)
+    }
+
+    def run(): Y2 = f(process.run())
+  }
+
+  case class SeqImpl[P1 <: Process[Y1], P2 <: Process[Y2], Y1, Y2](first: P1, andThen: Y1 => P2) extends Seq[P1, P2, Y2] {
+    override def run(): Y2 = {
+      println(s"*** Seq - step 1: running ${first}")
+      val ret = first.run()
+      val cont = andThen(ret)
+      println(s"*** Seq - step 2: running continuation ${cont}")
+      cont.run()
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Examples
+///////////////////////////////////////////////////////////////////////////////
+
+type Protocol0[C <: Channel[Int]] =
+  Yielding[Recv[C, Int], Int]
+
+def test0(c: Channel[Int])(implicit timeout: Duration): Protocol0[c.type] = for {
+  v <- receive(c)
+} yield v
+
+type Protocol1[C <: Channel[Int]] =
+  In[C, Int, (a: Int) =>
+    In[C, Int, (b: Int) =>
+      Yielding[Out[C, a.type], a.type],
+    Int],
+  Int]
+
+def test1(c: Channel[Int])(implicit timeout: Duration): Protocol1[c.type] = for {
+  v <- receive(c)
+  v2 <- receive(c)
+  _ <- send(c, v)
+} yield v
+
+def test1Approx(c: Channel[Int])(implicit timeout: Duration): Process[Int] = {
+  test1(c)
+}
+
+type Protocol1B[C <: Channel[Int]] = Protocol1[C]
+
+def test1b(c: Channel[Int])(implicit timeout: Duration): Protocol1[c.type] = for {
+  v <- test1(c)
+} yield v
+
+type Protocol2[C1 <: Channel[Int], C2 <: OutChannel[Unit], C3 <: OutChannel[Unit]] =
+  In[C1, Int, (a: Int) =>
+    In[C1, Int, (b: Int) =>
+      Seq[Out[C1, a.type],
+          Seq[Out[C2, Unit],
+              Yielding[Out[C3, Unit], (String, a.type)],
+          (String, Int)],
+      (String, Int)],
+    (String, Int)],
+  (String, Int)]
+
+def test2(c: Channel[Int], d: Channel[Unit], e: Channel[Unit])
+         (implicit timeout: Duration): Protocol2[c.type, d.type, e.type] = for {
+  a <- receive(c)
+  b <- receive(c)
+  x <- send(c, a)
+  _ <- send(d, ())
+  z <- send(e, ())
+} yield ("Hello", a)
+
+
+def test3(c: Channel[Int], d: Channel[Unit], e: Channel[Unit])
+         (implicit timeout: Duration): Process[(String, Int)] = for {
+  x <- test1(c)
+  _ <- send(d, ())
+  z <- send(e, ())
+} yield ("Hello", x)
+
+case class A();
+case class B();
+
+type Protocol3[C <: Channel[A|B]] =
+  Seq[Out[C,A], Protocol3Recv[C],
+      A | B | (String, B)]
+
+type Protocol3Recv[C <: Channel[A|B]] =
+  In[C, A|B, (v: A|B) => Protocol3Match[C, v.type],
+     A | B | (String, B)]
+
+type Protocol3Match[C <: Channel[A|B], X <: A|B] <: Process[A | B | (String, B)] = X match {
+  case A => Yielding[Out[C, X], X]
+  case B => Seq[Out[C, X],
+                Yielding[Out[C, X], (String, B)],
+                (String, B)]
+}
+
+type Protocol3Match2[C <: Channel[A|B], X <: A|B] <: Process[A | B | (String, B)] = X match {
+  case A => Yielding[Out[C, X], X]
+  case B => Seq[Out[C, X],
+                Yielding[Out[C, X], (String, B)],
+                (String, B)]
+}
+
+def test3(c: Channel[A|B])(implicit timeout: Duration): Protocol3[c.type] = for{
+  _ <- send(c, A())
+  ret <- test3recv(c)
+} yield ret
+
+def test3recv(c: Channel[A|B])(implicit timeout: Duration): Protocol3Recv[c.type] = {
+  receive(c)(timeout) { x => x match {
+    case _: A => for { _ <- send(c, x) } yield x
+    case y: B => for {
+      _ <- send(c, x)
+      _ <- send(c, x)
+    } yield ("Hello", y)
+  } }
+}
+
+def test3MatchB(c: OutChannel[A|B], x: A|B): Seq[Out[c.type, x.type], Yielding[Out[c.type, x.type], x.type], x.type] = for {
+  _ <- send(c, x)
+  _ <- send(c, x)
+} yield x
+
+def main(): Unit = {
+  implicit val timeout = Duration("5 seconds")
+
+  val c1 = Channel[Int]()
+  val c2 = Channel[Unit]()
+  val c3 = Channel[Unit]()
+
+  c1.send(42)
+  c1.send(43)
+  val p = test3(c1, c2, c3)
+  val res = p.run()
+  println(s"Result: ${res}")
+
+  val c4 = Channel[A|B]()
+
+  c4.send(B())
+  val res2 = test3(c4).run()
+  println(s"Result: ${res2}")
+}
